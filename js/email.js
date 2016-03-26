e1e2b703de2d9e492f09825b16b5ff0c8a636e7a
Cody Childers <cchilder@mail.usf.edu>
2016-03-26 10:41:20 -0500

    added headers

--- js/email.js
+++ js/email.js
@@ -7,10 +7,12 @@ $('.new-email-button').click(function() {
     console.log(message);
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(fromEmail)) {
-    alert(fromEmail);
-    $.ajax({
+        $.ajax({
             type: "POST",
             url: "https://api.postmarkapp.com/email",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+            }
             data: {
               "From": "info@pythondevhouse.com",
               "To": "cchilder@mail.usf.edu",
@@ -18,27 +20,23 @@ $('.new-email-button').click(function() {
               "Bcc": "blank-copied@example.com",
               "Subject": "Test",
               "Tag": "Invitation",
-              "HtmlBody": "<b>Hello</b>",
-              "TextBody": "Hello",
+              "TextBody": message,
               "ReplyTo": fromEmail,
               "Headers": [
                 { 
-                    "Name": "X-Postmark-Server-Token", 
+                    "Name": "X-Postmark-Server-Token",
                     "Value": "ba7663a2-19ba-4a42-bf69-5b4485fcab6f"
-                  },
-                  { 
-                    "Name": "Content-Type", 
-                    "Value": "application/json"
-                },
-                { 
-                    "Name": "Accept", 
-                    "Value": "application/json"
                 }
               ],
               "TrackOpens": true
-            }    
+            },
+            contentType: "application/json",
+            accepts: {
+                text: "application/json"
+            }
             
            }).done(function(response) {
+             alert(response);
              if (response[0].status == 'rejected'){
                alert("I'm sorry, the email form isn't working.\n\nPlease email info@pythondevhouse.com\nor call 813-545-2150");
              }

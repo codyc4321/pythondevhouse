6fbbaa2072355adb15f0e5c52874494831095901
Cody Childers <cchilder@mail.usf.edu>
2016-03-26 20:44:09 -0500

    added floorplan

--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
+from .forms import MessageForm
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    print(app.root_path)
+    form = MessageForm()
     return render_template('index.html',
                            title='Learn Python where you live',
+                           form=form
+                           )
+                           
+@app.route('/floorplan', methods=['GET'])
+def floorplan():
+    return render_template('floorplan.html',
+                           title='Floorplan'
                            )

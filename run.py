53902f5b8a965bd94203f611493f16592bde0f88
Cody Childers <cchilder@mail.usf.edu>
2016-04-03 21:52:47 -0500

    abstract templates

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+
 #!/usr/bin/env python
     
 from app import application
+from app.template_tags import render_collaborative, render_skill
+
+application.jinja_env.globals["render_collaborative"] = render_collaborative
+application.jinja_env.globals["render_skill"] = render_skill
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)

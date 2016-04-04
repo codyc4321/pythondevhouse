48710df43a4cfab591a1c0ba620e5085624a7d5e
Angelo Scalabroni <armotek5421@msn.com>
2016-04-03 22:41:07 -0400

    add basic Ajax inquiry form validation; before integrating on server side to send actual email

--- app/static/js/email.js
+++ app/static/js/email.js
@@ -1,55 +1,55 @@
-$('.new-email-button').click(function() {
+// $('.new-email-button').click(function() {
 
-    var fromEmail = $('.email').val();
-    console.log('email');
-    console.log(fromEmail);
-    var message = $('.email-message').val();
-    console.log(message);
-    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (re.test(fromEmail)) {
-        $.ajax({
-            type: "POST",
-            url: "https://api.postmarkapp.com/email",
-            // headers: {
-            //     "Access-Control-Allow-Origin": "http://codyc4321.github.io",
-            // }
-            data: {
-              "From": "info@pythondevhouse.com",
-              "To": "cchilder@mail.usf.edu",
-              "Cc": "copied@example.com",
-              "Bcc": "blank-copied@example.com",
-              "Subject": "Test",
-              "Tag": "Invitation",
-              "TextBody": message,
-              "ReplyTo": fromEmail,
-              "Headers": [
-                { 
-                    "Name": "X-Postmark-Server-Token",
-                    "Value": "ba7663a2-19ba-4a42-bf69-5b4485fcab6f"
-                }
-              ],
-              "TrackOpens": true
-            },
-            contentType: "application/json",
-            accepts: {
-                text: "application/json"
-            }
+//     var fromEmail = $('.email').val();
+//     console.log('email');
+//     console.log(fromEmail);
+//     var message = $('.email-message').val();
+//     console.log(message);
+//     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     if (re.test(fromEmail)) {
+//         $.ajax({
+//             type: "POST",
+//             url: "https://api.postmarkapp.com/email",
+//             // headers: {
+//             //     "Access-Control-Allow-Origin": "http://codyc4321.github.io",
+//             // }
+//             data: {
+//               "From": "info@pythondevhouse.com",
+//               "To": "cchilder@mail.usf.edu",
+//               "Cc": "copied@example.com",
+//               "Bcc": "blank-copied@example.com",
+//               "Subject": "Test",
+//               "Tag": "Invitation",
+//               "TextBody": message,
+//               "ReplyTo": fromEmail,
+//               "Headers": [
+//                 { 
+//                     "Name": "X-Postmark-Server-Token",
+//                     "Value": "ba7663a2-19ba-4a42-bf69-5b4485fcab6f"
+//                 }
+//               ],
+//               "TrackOpens": true
+//             },
+//             contentType: "application/json",
+//             accepts: {
+//                 text: "application/json"
+//             }
             
-           }).done(function(response) {
-             alert(response);
-             if (response[0].status == 'rejected'){
-               alert("I'm sorry, the email form isn't working.\n\nPlease email info@pythondevhouse.com\nor call 813-545-2150");
-             }
-             else{
-               alert("Sent successfully! Thank you, I will contact you soon");
-               console.log(response); // I am.
-             }
-             if(response[0].status === "sent") {
-               $('.email').val('');
-               $('.email-message').val('');
-             }
-           });
-      } else {
-        alert("ERROR...please check form and resubmit");
-      }
-});
+//            }).done(function(response) {
+//              alert(response);
+//              if (response[0].status == 'rejected'){
+//                alert("I'm sorry, the email form isn't working.\n\nPlease email info@pythondevhouse.com\nor call 813-545-2150");
+//              }
+//              else{
+//                alert("Sent successfully! Thank you, I will contact you soon");
+//                console.log(response); // I am.
+//              }
+//              if(response[0].status === "sent") {
+//                $('.email').val('');
+//                $('.email-message').val('');
+//              }
+//            });
+//       } else {
+//         alert("ERROR...please check form and resubmit");
+//       }
+// });
